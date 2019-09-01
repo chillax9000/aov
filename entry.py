2c5500d719b0ed94866369f90b8bb45f3e4d5f05
lpc <lpintocastaneda@gmail.com>
2019-09-01 11:46:06 +0200

    small
    + import datetime in entry renamed dt

--- entry.py
+++ entry.py
@@ -1,8 +1,8 @@
-import datetime
+import datetime as dt
 
 
 class Entry:
-    def __init__(self, text="", datetime=datetime.datetime(2000, 1, 1)):
+    def __init__(self, text="", datetime=dt.datetime(2000, 1, 1)):
         self.text = text
         self.datetime = datetime
 
@@ -10,4 +10,4 @@ class Entry:
     def from_file(cls, file_path):
         with open(file_path, "r") as f:
             text = f.read()
-            return cls(text, datetime.datetime.now())
+            return cls(text, dt.datetime.now())
