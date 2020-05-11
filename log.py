f82d4067849a9714dd82b65ff59ff81b76a250d9
lpc <lpintocastaneda@gmail.com>
2020-05-12 00:56:14 +0200

    base class?

--- log.py
+++ log.py
@@ -9,26 +9,31 @@ PREFIX = {
 
 
 class State(enum.Enum):
-    unitialized = 1
-    ongoing = 2
-
-    ended = -1
-    error = -2
-
-    def __bool__(self):
-        return bool(self.value)
+    unitialized = 0
+    ongoing = 1
+    ended = 2
 
 
-class ListUnique:
-    def __init__(self, child_class, handler, start_symbol=None, end_symbol=None):
+class Base:
+    def __init__(self, handler=None, state=State.unitialized):
         self.handler = handler
-        self.state = State.unitialized
+        self.state = state
+
+    def process(self, line):
+        if self.state is State.ended:
+            raise Exception(f"{self} is ended but was called")
+        return self._process(line)
+
+
+class ListUnique(Base):
+    def __init__(self, child_class, handler, start_symbol=None, end_symbol=None):
+        super().__init__(handler)
         self.start_symbol = start_symbol
         self.end_symbol = end_symbol
         self.child_class = child_class
         self.children = []
 
-    def deal_with(self, line):
+    def _process(self, line):
         if self.state is State.unitialized:
             self.state = State.ongoing
             if self.start_symbol is not None:
@@ -42,21 +47,21 @@ class ListUnique:
             self.children.append(self.child_class(self))
             return False, self.children[-1]
 
-        raise Exception(f"{self} is ended but was called")
-
     def __repr__(self):
         return f"<ListOnly {self.children}>"
 
+    def __iter__(self):
+        yield from self.children
 
-class Span:
+
+class Span(Base):
     def __init__(self, prefix, collect, inital_value, handler):
-        self.handler = handler
-        self.state = State.unitialized
+        super().__init__(handler)
         self.collect = collect
         self.prefix = prefix
         self.value = inital_value
 
-    def deal_with(self, line):
+    def _process(self, line):
         if self.state is State.unitialized:
             self.state = State.ongoing
         if self.state is State.ongoing:
@@ -67,17 +72,14 @@ class Span:
                 self.state = State.ended
                 return False, self.handler
 
-        raise Exception(f"{self} is ended but was called")
 
-
-class Entry:
+class Entry(Base):
     def __init__(self, handler):
-        self.handler = handler
-        self.state = State.unitialized
+        super().__init__(handler)
         self.time = None
         self.text = Span(PREFIX["Text"], (lambda x, y: x + y), "", self)
 
-    def deal_with(self, line):
+    def _process(self, line):
         if self.state is State.unitialized:
             self.state = State.ongoing
             prefix = PREFIX["Entry"]
@@ -87,13 +89,18 @@ class Entry:
                 raise Exception(f"{self} expected first line to start with {prefix}")
             return True, self.text
         if self.state is State.ongoing:
+            self.text = self.text.value
             self.state = State.ended
             return False, self.handler
 
-        raise Exception(f"{self} is ended but was called")
-
     def __repr__(self):
-        return f"<Entry {self.time}, {repr(self.text.value[:32])}>"
+        return f"<Entry {self.time}, {repr(self.text)[:32]}>"
+
+
+def format_entry(entry, pfx_entry=PREFIX["Entry"], pfx_text=PREFIX["Text"]):
+    yield pfx_entry + entry.time.isoformat()
+    for line in entry.text.split("\n"):
+        yield pfx_text + line
 
 
 def empty(line):
@@ -110,7 +117,7 @@ def file_ended(line):
 
 def parse_base(file_path):
     if not os.path.exists(file_path):
-        raise FileNotFoundError(f"No file found at {file_path}")
+        raise FileNotFoundError(file_path)
 
     entries = ListUnique(Entry, None)
     agent = entries
@@ -121,7 +128,7 @@ def parse_base(file_path):
                 line = f.readline()
                 continue
 
-            was_processed, next_agent = agent.deal_with(line)
+            was_processed, next_agent = agent.process(line)
             if next_agent is None:
                 if file_ended(line):
                     break
@@ -135,4 +142,9 @@ def parse_base(file_path):
 
 
 if __name__ == "__main__":
-    print(parse_base("log_test"))
+    es = parse_base("logtest")
+    print(es)
+    print("...")
+    for entry in es:
+        for line in format_entry(entry):
+            print(line)
